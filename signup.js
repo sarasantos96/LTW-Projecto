20fe6af456db7df31a2a86f43ae83dc1e0c29ab1
Sara Santos <santos.sara96@gmail.com>
2016-12-05 17:27:36 +0000

    CSS para sign up warnings

--- signup.js
+++ signup.js
@@ -6,22 +6,22 @@ function submitForm() {
 	var name_check = true;
 	var username_check = true;
 	var password_check = true;
-	
+
 	var name = document.forms["signup_form"]["name"].value;
 	if (name == "") {
 		name_check = false;
 	}
-	
+
 	var username = document.forms["signup_form"]["username"].value;
 	if (username == "") {
 		username_check = false;
 	}
-	
+
 	var pass = document.forms["signup_form"]["password"].value;
 	if (pass == "") {
 		password_check = false;
 	}
-	
+
 	if(!name_check || !username_check || !password_check){
 		var names = [];
 		var warning;
@@ -31,7 +31,7 @@ function submitForm() {
 			names.push("username");
 		if(!password_check)
 			names.push("password");
-		
+
 		if(names.length == 1){
 			warning = "The " + names[0] + " parameter must be filled";
 		}
@@ -40,37 +40,37 @@ function submitForm() {
 		}
 		if(names.length == 3)
 			warning = "The " + names[0] + ", " + names[1] + " and " +  names[2] + " parameters must be filled";
-		
+
 		document.getElementById("warnings").innerHTML = warning;
-		document.getElementById("signUpButton").style.top = 95+'px';
-		
+		document.getElementById("signUpButton").style.top = 110+'px';
+
 		return false;
 	}
-	
+
 }
 
 /*
-function submitForm() {	
+function submitForm() {
 	var name = document.forms["signup_form"]["name"].value;
 	if (name == "") {
 		document.getElementById("signUpButton").style.top = 60+'px';
 		document.getElementById("warnings").innerHTML = "Name must be filled out";
 		return false;
 	}
-	
+
 	var username = document.forms["signup_form"]["username"].value;
 	if (username == "") {
 		document.getElementById("signUpButton").style.top = 60+'px';
 		document.getElementById("warnings").innerHTML = "Username must be filled out";
 		return false;
 	}
-	
+
 	var pass = document.forms["signup_form"]["password"].value;
 	if (pass == "") {
 		document.getElementById("signUpButton").style.top = 60+'px';
 		document.getElementById("warnings").innerHTML = "Password must be filled out";
 		return false;
 	}
-	
+
 }
 */
