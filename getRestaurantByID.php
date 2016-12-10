62e18b3820fbe97724d305ad085947e52b951290
Nuno Miguel <nuno.migcastro@gmail.com>
2016-12-10 17:33:51 +0000

    Ficheiros extra apagados

--- getRestaurantByID.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$db = new PDO('sqlite:restaurant.db');
-	$id = $_GET['id'];
-	
-	$restaurant = $db->prepare('SELECT * FROM Restaurant WHERE RestaurantID = ?');
-	$restaurant->bindValue(1, "$id", PDO::PARAM_STR);
-	$restaurant->execute();
-	$restData = $restaurant->fetch();
-?>
