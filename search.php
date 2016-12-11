7305e04be19d91078a935502a5f7c8681feef339
Nuno Miguel <nuno.migcastro@gmail.com>
2016-12-11 14:58:38 +0000

    Search corrigido

--- search.php
+++ search.php
@@ -11,44 +11,28 @@
 
 	<body>
 		<?php include_once('templates/header.php'); ?>
-		  <div id= "signOptions">
-		  <?php if(isset($_SESSION['username']) && $_SESSION['username'] != null) { ?>
-			<ul>
-			   <li> <a href="userPage.php"> Hello <?= $_SESSION['username'] ?> </a> </li>
-			   <li> <a href="database/logout.php"> Log Out </a> </li>
-			</ul>
-		  <?php } else { ?>
-			<ul>
-				  <li> <a href="login.php"> Log In </a> </li>
-				  <li> <a href="signup.php"> Sign Up </a> <li>
-			</ul>
-		  <?php } ?>
+		<div id= "signOptions">
+			<?php if(isset($_SESSION['username']) && $_SESSION['username'] != null) { ?>
+				<ul>
+				   <li> <a href="userPage.php"> Hello <?= $_SESSION['username'] ?> </a> </li>
+				   <li> <a href="database/logout.php"> Log Out </a> </li>
+				</ul>
+			<?php } else { ?>
+				<ul>
+					  <li> <a href="login.php"> Log In </a> </li>
+					  <li> <a href="signup.php"> Sign Up </a> <li>
+				</ul>
+			<?php } ?>
 		</div>
 
 		<div id="result">
 			<?php include_once('database/searchRestaurants.php'); ?>
-			<?php if($cities == NULL): ?>
-				<?php if($restaurants == NULL): ?>
-					<div class="noResult">
-						<p> No search results for <?=$search?> </p>
-					</div>
-				<?php else: ?>
-					<?php foreach($restaurants as $row): ?>
-						<div class="restaurant">
-							<h2 name="restName">
-								<?=$count?>
-								<?=$row['Name']?>
-							</h2>
-							<p>
-								Address: <?=$row['Address']?></br>
-								Contact: <?=$row['PhoneNumber']?>
-							</p>
-						<?php $count = $count + 1; ?>
-						</div>
-					<?php endforeach; ?>
-				<?php endif ?>
+			<?php if($restaurants == NULL): ?>
+				<div class="noResult">
+					<p> No search results for <?=$search?> </p>
+				</div>
 			<?php else: ?>
-				<?php foreach($cities as $row): ?>
+				<?php foreach($restaurants as $row): ?>
 					<div class="restaurant">
 						<h2> <?=$count?>. <?=$row['ResName']?></h2>
 						<?php $count = $count + 1; ?>
