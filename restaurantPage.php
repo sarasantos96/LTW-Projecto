fa14364d94299c74b7f85a3955bf28d44138c93e
Sara Santos <santos.sara96@gmail.com>
2016-12-13 18:52:21 +0000

    css restaurant page updated + mapas

--- restaurantPage.php
+++ restaurantPage.php
@@ -10,30 +10,30 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 		<script type="text/javascript" src="addReview.js"></script>
 		<link rel="stylesheet" href="styles/addReview.css" >
-		
+
 		<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-		
-		
+
+
 		<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 		<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-		
+
 		<script>
 			$( function() {
 				var dialog, form,
-				
+
 				dialog = $( "#review_form_div" ).dialog({
 					autoOpen: false,
-					height: 400,
+					height: 300,
 					width: 350,
 					modal: true,
 					close: function() {
 						form[ 0 ].reset();
 					}
 				});
-			 
+
 				form = dialog.find( "#review_form" ).on( "submit", function( event ) {
 				});
-			 
+
 				$( "#review_button" ).button().on( "click", function() {
 				  dialog.dialog( "open" );
 				});
@@ -72,13 +72,17 @@
 				<p>Contact: <?=$restData['PhoneNumber']?></p></br>
 			</div>
 
+			<iframe id="map" src="http://www.map-embed.com/mapsv4.php?w=600&h=500&q=<?=$restData['Address']?>, &z=14&t=" width="500" height="300" frameborder="5"
+								scrolling="no" marginheight="0" marginwidth="0"><a class="addmaps" href="http://www.map-embed.com"id="get-map-data"
+								mce_href="http://maps.google.com/maps/api/js?sensor=false">map-embed.com</a><style>#gmap_canvas img{max-width:none!important;background:none!important}</style></iframe>
+
 			<div class="comments">
 				<h2 id="reviewsection"> Review Section </h2>
 
 				<div class="userInput" id="review_form_div">
 					<form id="review_form" name="review_form" class="edit" action="database/submitReview.php?id=<?php echo$_GET["id"]?>" method="post" onSubmit="return review_submit()">
 						<label id="name"> Review <br>
-							<input class="userInput" type="text" name="review" > <br>
+							<input class="reviewInput" type="text" name="review" > <br>
 						</label>
 
 						<div class="rating">
@@ -88,14 +92,14 @@
 							<span><input type="radio" name="rating" id="str2" value="2"><label for="str2"></label></span>
 							<span><input type="radio" name="rating" id="str1" value="1"><label for="str1"></label></span>
 						</div>
-						<input id = "signUpButton" type = "submit" value = "Submit Review">
+						<input id = "submitButton" type = "submit" value = "Submit Review">
 					</form>
 					<p id="warnings"></p>
 				</div>
 				<?php if(isset($_SESSION['username']) && $_SESSION['username'] != null) { ?>
 					<input id="review_button" type="button" onclick="review_click()" value="Add Review">
 				<?php } ?>
-				
+
 
 					<?php
 						$id = $_GET['id'];
@@ -108,9 +112,9 @@
 									<p id="username"><?php echo reviewerUsername($row['ReviewerID']);?> said: </p>
 									<h4 id="score"><?=$row['Score']?>/5</h4>
 									<p id="review"><?=$row['Review']?></p>
-									<a class="add_reply" onclick="addReply(<?=$row['ReviewID']?>)">Reply</a>
+									<a class="add_reply" onclick="addReply(<?=$row['ReviewID']?>)">Reply</a> <br>
 									<form class="reply_form_class" method="post" action="database/submitNewReply.php?reviewID=<?=$row['ReviewID']?>&id=<?=$id?>">
-										<input class="userInput" type="text" name="reply" > <br>
+										<input class="replyInput" type="text" name="reply" > <br>
 										<input id = "replyButton" type = "submit" value = "Submit Reply">
 									</form>
 								</div>
@@ -118,7 +122,7 @@
 									$replies = reviewReplies($row['ReviewID']);
 									if($replies != NULL):
 										foreach($replies as $reply): ?>
-											<div class="review">
+											<div class="restaurantreply">
 												<p class="restaurantName"> <?=$restaurantName?> said:</p>
 												<p class="reply"><?=$reply['Reply']?></p>
 											</div>
